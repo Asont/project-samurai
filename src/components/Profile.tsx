d01cb24a1861ac02ed6bf17bf3e2b073b3a18a04
Asont <a.druzhko@gmail.com>
2022-01-08 19:42:38 +0300

    create module css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import './Profile.css'
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img src="https://igate.com.ua/upload/photo/0001/0001/1717/8266/89.jpg" alt="image" height="300px"/>
             </div>
             <div>ava + discription</div>
             <div>My posts</div>
             <div>New posts</div>
-            <div>
-                <div>post 1</div>
-                <div>post 2</div>
+            <div className={s.posts}>
+                <div className={s.item}>post 1</div>
+                <div className={s.item}>post 2</div>
             </div>
         </div>
     );
