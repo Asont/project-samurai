4a6cbd5b34b2b82f54aa2754de934e05dc6959ec
Asont <a.druzhko@gmail.com>
2022-01-08 18:33:21 +0300

    message

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://igate.com.ua/upload/photo/0001/0001/1717/8266/89.jpg" alt="image" height="300px"/>
+            </div>
+            <div>ava + discription</div>
+            <div>My posts</div>
+            <div>New posts</div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
