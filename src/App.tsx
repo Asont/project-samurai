c5e0d6835db0140f0c09ff8c038d6ba14d291ca1
Asont <a.druzhko@gmail.com>
2022-01-15 10:24:35 +0300

    create <Route> <BrowserRouter> <Routes> <Navbar to>

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,33 @@ import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
 import Dialogs from "./components/dialogs/Dialogs";
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const alex = {
+    name:"Alex"
+}
+
+let copyAlex = {...alex}
+
+copyAlex.name="John"
+
+console.log(alex)
+console.log(copyAlex)
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header />
             <Navbar />
-            <div className="app-wrapper-content">
-                {/*<Dialogs />*/}
-                <Profile />
+            <div className="app-wrapper-content" >
+                <Routes>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/dialogs"  element={<Dialogs/>}/>
+            </Routes>
             </div>
-            {/* <Profile />*/}
         </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css'
 
 
@@ -6,10 +7,10 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href="/components/profile/Profile.tsx">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.atcive}`}>
-                <a href="/components/dialogs/Dialogs.tsx">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a>News</a>
